e4ddbd28d8f83aaebe66c679318375dbc1eb7ad6
ziqizhang <ziqizhang.email@googlemail.com>
2015-09-23 11:31:59 +0100

    .

--- src/uk/ac/shef/dcs/jate/v2/app/Indexing.java
+++ src/uk/ac/shef/dcs/jate/v2/app/Indexing.java
@@ -1,6 +1,7 @@
 package uk.ac.shef.dcs.jate.v2.app;
 
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import uk.ac.shef.dcs.jate.v2.JATEException;
 import uk.ac.shef.dcs.jate.v2.JATEProperties;
@@ -18,21 +19,27 @@ import java.util.logging.Logger;
  * Created by zqz on 23/09/2015.
  */
 public class Indexing {
-    public static void main(String[] args) throws IOException, JATEException {
+    public static void main(String[] args) throws IOException, JATEException, SolrServerException {
         Logger logger = Logger.getLogger(Indexing.class.getName());
-        logger.info("INDEXING BEGINS");
         JATEProperties prop = new JATEProperties(args[0]);
+        boolean deletePrevious = Boolean.valueOf(args[2]);
         SolrClient solrClient =
                 new EmbeddedSolrServer(Paths.get(prop.getSolrHome()),
                         prop.getSolrCorename());
+        if (deletePrevious) {
+            logger.info("DELETING PREVIOUS INDEX");
+            solrClient.deleteByQuery("*:*");
+            solrClient.commit();
+        }
+        logger.info("INDEXING BEGINS");
         SolrParallelIndexingMaster m = new SolrParallelIndexingMaster();
         List<String> files = new ArrayList<>();
-        for(File f: new File(args[1]).listFiles())
+        for (File f : new File(args[1]).listFiles())
             files.add(f.toString());
         m.index(files, prop.getIndexerMaxDocsPerWorker(),
                 prop.getIndexerMaxUnitsToCommit(),
-                new TikaSimpleDocumentCreator(),solrClient,
-                prop.getFeatureBuilderMaxCPUsage(),prop);
+                new TikaSimpleDocumentCreator(), solrClient,
+                prop.getFeatureBuilderMaxCPUsage(), prop);
         logger.info("INDEXING COMPLETE");
         System.exit(0);
     }
