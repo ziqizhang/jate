7530cadb70cc4118436a58ae27d7805188752c83
ziqizhang <ziqizhang.email@googlemail.com>
2015-09-29 16:21:38 +0100

    .

--- src/uk/ac/shef/dcs/jate/algorithm/TermInfoCollector.java
+++ src/uk/ac/shef/dcs/jate/algorithm/TermInfoCollector.java
@@ -1,22 +1,49 @@
 package uk.ac.shef.dcs.jate.algorithm;
 
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.*;
+import org.apache.lucene.util.BytesRef;
+import uk.ac.shef.dcs.jate.model.JATEDocument;
 import uk.ac.shef.dcs.jate.model.TermInfo;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  */
-public class TermInfoCollector{
+public class TermInfoCollector {
 
     protected IndexReader indexReader;
+    protected String ngramInfoFieldname;
+    protected String idFieldname;
 
-    public TermInfoCollector(IndexReader indexReader){
-        this.indexReader=indexReader;
+    public TermInfoCollector(IndexReader indexReader) {
+        this.indexReader = indexReader;
     }
 
-    public TermInfo collect(String term){
+    public TermInfo collect(String term) throws IOException {
+        TermInfo info = new TermInfo();
 
-        //todo
-        return null;
+        PostingsEnum postings =
+                MultiFields.getTermDocsEnum(indexReader, ngramInfoFieldname, new BytesRef(term.getBytes()));
+
+        int docId = postings.nextDoc();
+        while (docId != PostingsEnum.NO_MORE_DOCS) {
+            Document doc = indexReader.document(docId);
+            String id = doc.get(idFieldname);
+            JATEDocument jd = new JATEDocument(id);
+            Set<int[]> offsets = new HashSet<>();
+            int totalFreq = postings.freq();
+            for (int i = 0; i < totalFreq; i++) {
+                postings.nextPosition();
+                offsets.add(new int[]{postings.startOffset(), postings.endOffset()});
+            }
+            info.getOffsets().put(jd, offsets);
+
+        }
+
+        return info;
     }
 }
