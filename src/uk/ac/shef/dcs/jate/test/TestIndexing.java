1b32314843d1b0748365c69f1d43a310ee83a1d3
ziqizhang <-@s-MacBook-Pro.local>
2015-10-12 11:30:29 +0100

    .

--- src/uk/ac/shef/dcs/jate/test/TestIndexing.java
+++ src/uk/ac/shef/dcs/jate/test/TestIndexing.java
@@ -4,15 +4,49 @@ import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
+import uk.ac.shef.dcs.jate.JATEException;
+import uk.ac.shef.dcs.jate.io.DocumentCreator;
+import uk.ac.shef.dcs.jate.io.TikaSimpleDocumentCreator;
+import uk.ac.shef.dcs.jate.model.JATEDocument;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zqz on 04/10/2015.
  */
 public class TestIndexing {
-    public static void main(String[] args) throws IOException, SolrServerException {
+    public static void main(String[] args) throws IOException, SolrServerException, JATEException {
+
+        AutoDetectParser parser = new AutoDetectParser();
+        for (File f : new File("/Users/-/jate/experiment/acl-rd-corpus").listFiles()) {
+            if(f.toString().contains("10228")||f.toString().contains("_15.txt")||f.toString().contains("9113"))
+                System.out.println();
+
+
+            InputStream in = new BufferedInputStream(new FileInputStream(f.toString()));
+            BodyContentHandler handler = new BodyContentHandler(-1);
+            Metadata metadata = new Metadata();
+            try {
+
+                parser.parse(in, handler, metadata);
+                String content = handler.toString();
+
+                if (content.length() == 0)
+                    System.out.println(f);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        System.exit(0);
+
+
+
         SolrClient solrClient =
                 new EmbeddedSolrServer(Paths.get("D:\\Work\\jate_github\\jate\\solr-5.3.0_\\server\\solr"),
                         "core1");
